11519d9337db03d960b2eb6f62d932692d1a29fe
aniamarkh <aniamarkh@gmail.com>
2023-06-22 22:02:45 +0300

    refactor: page title, styles

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
-import './assets/style.css'
+import './assets/style.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-
-app.mount('#app')
+app.mount('#app');
